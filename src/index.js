574ce66f2fab09f290605aa64d622a76de902d7f
Ben Tucker <bwtucker4@gmail.com>
2022-06-01 11:47:58 -0700

    Add menu button func, add form func

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./style.css";
+
+const buttonToggles = (() => {
+    const menuBtn = document.getElementById("menu-btn");
+    const navBar = document.getElementById("nav");
+    let navDisplay = true;
+    menuBtn.addEventListener("click", function() {
+        if (navDisplay == true) {
+            navBar.style.display = "none";
+            document.querySelector("main").style.width = "100vw";
+            navDisplay = false;
+        } else {
+            navBar.style.display = "block";
+            document.querySelector("main").style.width = "auto";
+            navDisplay = true;
+        }
+    })
+
+    const newTaskBtn = document.getElementById("add-task");
+    let formDisplay = false;
+    newTaskBtn.addEventListener("click", function() {
+        if (formDisplay == false) {
+            document.getElementById("form-container").style.display = "flex";
+            formDisplay = true;
+        } else {
+            return;
+        }
+    })
+
+    const cancelFormBtn = document.getElementById("hide-form");
+    cancelFormBtn.addEventListener("click", function() {
+        if (formDisplay == true) {
+            document.getElementById("form-container").style.display = "none";
+            formDisplay = false;
+        }
+    })
+})();
