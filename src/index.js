76b28053e4c5640f85e096a318ec76e78fe94fa1
Ben Tucker <bwtucker4@gmail.com>
2022-06-03 15:54:59 -0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 let myTasks = [];
 
+// Task creation
 class Task {
     constructor(title, details, date) {
         this.title = title;
@@ -10,6 +11,7 @@ class Task {
     }
 }
 
+// Toggle nav bar display, show/hide task form
 const buttonToggles = (() => {
     const menuBtn = document.getElementById("menu-btn");
     const navBar = document.getElementById("nav");
@@ -46,61 +48,58 @@ const buttonToggles = (() => {
     })
 })();
 
-const addTaskToArray = (() => {
-    const form = document.getElementById("form");
-    const submitTaskBtn = document.getElementById("submit-task");
+// Add task to array when form is submitted
+const form = document.getElementById("form");
+const submitTaskBtn = document.getElementById("submit-task");
+submitTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const title = document.getElementById("task-title").value;
+    const details = document.getElementById("task-details").value;
+    const date = document.getElementById("task-date").value;
 
-    submitTaskBtn.addEventListener("click", function(e) {
-        e.preventDefault();
-        const title = document.getElementById("task-title").value;
-        const details = document.getElementById("task-details").value;
-        const date = document.getElementById("task-date").value;
-    
-        let newTask = new Task(title, details, date);
-        myTasks.push(newTask);
-        form.reset();
-        console.log(myTasks);
+    let newTask = new Task(title, details, date);
+    myTasks.push(newTask);
+    renderTasks();
+    form.reset();
+    console.log(myTasks);
+})
+
+
+// Display current tasks on screen
+function renderTasks() {
+    const taskList = document.getElementById("task-list");
+    const allTasks = document.querySelectorAll(".task-item");
+    for (let i = 0; i < allTasks.length; i++) {
+        allTasks[i].remove();
+    }
+
+    myTasks.forEach((task) => {
+        const taskItem = document.createElement("div");
+        taskItem.classList.add("task-item");
+        taskList.appendChild(taskItem);
+
+        const displayTitle = document.createElement("h4");
+        displayTitle.classList.add("displayTitle");
+        displayTitle.textContent = task.title;
+        taskItem.appendChild(displayTitle);
+
+        const displayDetails = document.createElement("p");
+        displayDetails.classList.add("displayDetails");
+        displayDetails.textContent = task.details;
+        taskItem.appendChild(displayDetails);
+
+        const displayDate = document.createElement("p");
+        displayDate.classList.add("displayDate");
+        displayDate.textContent = task.date;
+        taskItem.appendChild(displayDate);
+
+        const editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        taskItem.appendChild(editButton);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.setAttribute("id", "delete");
+        deleteButton.textContent = "Delete";
+        taskItem.appendChild(deleteButton);
     })
-})();
-
-const renderTasks = (() => {
-    let index = 0;
-    const submitTaskBtn = document.getElementById("submit-task");
-
-    submitTaskBtn.addEventListener("click", function() {
-        const taskList = document.getElementById("task-list");
-        const allTasks = document.querySelectorAll(".task-item");
-        for (let i = 0; i < allTasks.length; i++) {
-            allTasks[i].remove();
-        }
-
-        myTasks.forEach((task) => {
-            const taskItem = document.createElement("div");
-            taskItem.classList.add("task-item");
-            taskList.appendChild(taskItem);
-
-            const displayTitle = document.createElement("h4");
-            displayTitle.classList.add("displayTitle");
-            displayTitle.textContent = task.title;
-            taskItem.appendChild(displayTitle);
-
-            const displayDetails = document.createElement("p");
-            displayDetails.classList.add("displayDetails");
-            displayDetails.textContent = task.details;
-            taskItem.appendChild(displayDetails);
-
-            const displayDate = document.createElement("p");
-            displayDate.classList.add("displayDate");
-            displayDate.textContent = task.date;
-            taskItem.appendChild(displayDate);
-
-            const editButton = document.createElement("button");
-            editButton.textContent = "Edit";
-            taskItem.appendChild(editButton);
-
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Delete";
-            taskItem.appendChild(deleteButton);
-        })
-    })
-})();
+}
